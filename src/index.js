5d3fb4a7e56c6a3f8facc9e5710b9dc28085c38d
justasti <justas.tirevicius@gmail.com>
2023-06-05 20:15:23 +0300

    feat: implement stripe payments

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.scss'
 import { PersistGate } from 'redux-persist/integration/react'
+import { Elements } from '@stripe/react-stripe-js'
 
 import App from './App'
 import { Provider } from 'react-redux'
 import { persistor, store } from './store/store'
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -14,7 +16,9 @@ root.render(
     <Provider store={store}>
       {/* <PersistGate persistor={persistor}> */}
         <BrowserRouter>
+        <Elements stripe={stripePromise}>
           <App />
+        </Elements>
         </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
